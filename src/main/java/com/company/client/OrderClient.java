ec6a164a3cf50c7fe2c00866244f0b03b2917971
nijatkhanaliev <nicatxanaliev@gmail.com>
2025-11-26 02:36:40 +0400

    modified orderClient

--- src/main/java/com/company/client/OrderClient.java
+++ src/main/java/com/company/client/OrderClient.java
@@ -3,14 +3,14 @@ package com.company.client;
 import com.company.model.dto.response.OrderResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.util.List;
 
 @FeignClient(name = "order-service", url = "${application.client.order}")
 public interface OrderClient {
 
-    @GetMapping("v1/orders/userId/{userId}")
-    List<OrderResponse> getAllOrderByUserId(@PathVariable Long userId);
+    @GetMapping("v1/orders")
+    List<OrderResponse> getAllOrderByUserId( @RequestHeader("current-user-id") Long userId);
 
 }
